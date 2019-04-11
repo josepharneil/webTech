fef9f87e81a07610d095acb14d232948a5faddac
Joseph Arneil <josepharneil@gmail.com>
2019-04-11 16:31:31 +0100

    installed mongoose

--- site/expressTutorial/express-locallibrary-tutorial/app.js
+++ site/expressTutorial/express-locallibrary-tutorial/app.js
@@ -13,15 +13,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Add mid dleware libraries
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//Serve all static files in /public
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Route-handling code
+//Defines particular routes for the different parts of the site
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//Final middleware adds handler methods for errors and HTTP 404 responses
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -38,4 +43,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//Now we have fully configured the Express application object, app
+//We finally add it to the module exports
+//This is what allows it to be imported by /bin/www
 module.exports = app;
